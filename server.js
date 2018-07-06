650e3001e8eb6cb4ce0b08cdb7a7c92a9178db14
manasi9307 <manasi.thakur@sjsu.edu>
2018-07-06 15:45:27 -0700

    Addition of project.hbs

--- server.js
+++ server.js
@@ -50,6 +50,12 @@ app.get('/about', (req, res)=>{
   });
 });
 
+app.get('/projects', (req, res)=>{
+  res.render('projects.hbs',{
+    pagetitle: 'Projects'
+  })
+});
+
 app.get('/bad', (req, res)=>{
   res.send({
     errorMessage:'Unable to send data'
